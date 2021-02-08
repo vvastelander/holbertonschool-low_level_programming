8c30eae5461b5354191e930dde0f15a9717ec9ca
vvastelander <selimbarbirou3301@gmail.com>
2021-02-08 15:36:55 +0000

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ *Return: 0
+ */
+int main(void)
+{
+char AZ;
+for (AZ = 'z'; AZ >= 'a'; AZ--)
+putchar(AZ);
+putchar('\n');
+return(0);
+}
