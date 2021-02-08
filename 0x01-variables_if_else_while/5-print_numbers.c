882bce7ac190631438e83c0077e679f1e5d3f1cb
vvastelander <selimbarbirou3301@gmail.com>
2021-02-08 14:56:58 +0000

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ *Return: 0
+ */
+int main(void)
+{
+int o;
+for (o = '0'; o <= '9'; o++)
+putchar (o);
+putchar ('\n');
+return (0);
+}
