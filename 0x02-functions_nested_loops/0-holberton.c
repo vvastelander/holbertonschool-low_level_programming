60e2f1a66f7123451682ebe98bb9916c408ef31e
vvastelander <selimbarbirou3301@gmail.com>
2021-02-09 14:00:02 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <holberton.h>
+/**
+ * main - entry point
+ * calling function _putchar
+ * Return: success
+ */
+int main(void)
+
+{
+_putchar('H')
+_putchar('o')
+_putchar('l')
+_putchar('b')
+_putchar('e')
+_putchar('r')
+_putchar('t')
+_putchar('o')
+_putchar('n')
+
+return (write(1, &c, 1));
+}
