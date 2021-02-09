16095aebbdebb49df601c486e44bfe548b6b1c68
vvastelander <selimbarbirou3301@gmail.com>
2021-02-09 11:35:22 +0000

    00-99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * prints 10 decimal numbers
+ * Return: success
+ */
+int main(void)
+{
+  int nu;
+  for(nu = 00; nu <= 99; nu++)
+    {
+      if (nu != 99)
+	putchar((nu / 10) + '0');
+      putchar((nu % 10) + '0');
+
+
+      {
+	putchar (',');
+	putchar (' ');
+      }
+    }
+  putchar ('\n');
+  return (0);
+}
