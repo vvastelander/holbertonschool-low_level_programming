870364fea4293ab4e918e276d10ad19b4cf20846
vvastelander <selimbarbirou3301@gmail.com>
2021-02-10 15:27:17 +0000

    10alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *print_alphabet_x10 - print alphabet 10times
+ *Return: success
+ */
+void print_alphabet_x10(void)
+{
+int c;
+char b;
+for (c = 0; c <= 10; c++)
+{
+for (b = 'a'; b <= 'z'; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+}
+}
