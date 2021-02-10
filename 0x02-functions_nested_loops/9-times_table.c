13445b207154fd3df463cd8d3be4466e3827e44d
vvastelander <selimbarbirou3301@gmail.com>
2021-02-10 22:51:38 +0000

    add

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+/**
+ * times_table - x9 table
+ */
+void times_table(void)
+{
+int i, j, x;
+
+x = 0;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+x = i * j;
+if (x < 10)
+{
+if (j <= 0)
+{
+_putchar((x % 10) + '0');
+}
+else
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+_putchar((x % 10) + '0');
+}
+}
+else
+{
+if (j <= 0)
+{
+_putchar((x / 10) + '0');
+_putchar((x % 10) + '0');
+}
+else
+{
+_putchar(44);
+_putchar(32);
+_putchar((x / 10) + '0');
+_putchar((x % 10) + '0');
+}
+}
+}
+_putchar('\n');
+}
+}
