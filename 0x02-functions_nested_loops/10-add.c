d3049cce738b9c6c06d7db474cbcd01ad3158bfe
vvastelander <selimbarbirou3301@gmail.com>
2021-02-11 09:17:17 +0000

    ttwoints

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *add - add two integers
+ *@a: variable a
+ *@b: variable b
+ *Return: c
+ */
+int add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
