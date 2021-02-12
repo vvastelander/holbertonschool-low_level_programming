9079adabbfb69f4f92521bb5060cc53bee47d585
vvastelander <selimbarbirou3301@gmail.com>
2021-02-12 12:19:41 +0000

    09

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *print_numbers - from 0 to 9
+ *@n: 09
+ *Return: num
+ */
+void print_numbers(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+  {
+_putchar (n);
+  }
+_putchar ('\n');
+return;
+}
+
