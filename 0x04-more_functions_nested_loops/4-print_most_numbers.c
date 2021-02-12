c4db6a911140ef7f71e4e8b55b123968502b31ad
vvastelander <selimbarbirou3301@gmail.com>
2021-02-12 13:49:19 +0000

    2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *print_most_numbers - =!24
+ *
+ *Return: print
+ */
+void print_most_numbers(void)
+{ 
+int r;
+for (r = 0; r <= 9; r++)
+{
+if ((r != '2') && (r != '4'))
+{
+_putchar (r);
+}
+}
+_putchar ('\n');
+}
