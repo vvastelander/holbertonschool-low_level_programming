4bb16020428b91df7e98c2c675e84caea8adf5a2
vvastelander <selimbarbirou3301@gmail.com>
2021-02-20 16:21:27 +0000

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,28 +1,37 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *print_to_98 - main function
- *
- *@n: print numbers to 98
- *
- *Return: void
+ * print_to_98 - prints last digit
+ * @n: parameter
+ * Return: success
  */
 void print_to_98(int n)
 {
 int i;
-if (n > 98)
+
+if (n == 98)
 {
-for (i = n; i >= 98; i--)
+printf("98\n");
+}
+else if (n <= 98)
 {
-printf("%i, ", i);
+for (i = n; i <= 98; i++)
+{
+if (i != 98)
+printf("%d, ", i);
+else
+printf("%d\n", i);
 }
 }
 else
 {
-for (i = n; i <= 98; i++)
+for (i = n; i >= 98; i--)
 {
-printf("%i, ", i);
+if (i != 98)
+printf("%d, ", i);
+else
+printf("%d\n", i);
 }
 }
-return;
+}
 }
